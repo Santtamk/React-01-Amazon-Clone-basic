0765b8033cd1fec5e405c0e41dfabde2b0350934
Santam Kumai <santtamk@gmail.com>
2025-02-04 16:43:57 +0530

    increment and decrement added in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import { useCart } from "../../context/CartContext";
 
 const Cart = () => {
+  const [count, setCount] = useState({});
   const { cartItems } = useCart();
-  console.log("cartItems from cart:", cartItems);
+  //   console.log("cartItems from cart:", cartItems);
+
+  const increment = (id) => {
+    setCount((prevCount) => ({
+      ...prevCount,
+      [id]: (prevCount[id] || 1) + 1,
+    }));
+  };
+
+  const decrement = (id) => {
+    setCount((prevCount) => ({
+      ...prevCount,
+      [id]: Math.max((prevCount[id] || 1) - 1, 1),
+    }));
+  };
+
   return (
     <div>
-      <div>Cart Items</div>
+      <h1>Cart Items</h1>
       {cartItems.map((item) => (
-        <div key={item.id}>
-          <div>
-            <img src={item.image} alt="" className="w-10 h-10" />
+        <div
+          key={item.id}
+          className="flex gap-5 p-5 w-full justify-center border-amber-600 "
+        >
+          <div className="w-40 h-40 flex text-ellipsis overflow-hidden whitespace-nowrap justify-center items-center flex-col">
+            <img src={item.image} alt="" className="w-30 h-30" />
+            <p className="">{item.title}</p>
           </div>
-          <div>+</div>
-          <div>1</div>
-          <div>-</div>
-          <div>{item.price}</div>
+          <div className="flex gap-5 items-center">
+            <button className="pointer" onClick={() => decrement(item.id)}>
+              -
+            </button>
+            <div>{count[item.id] || 1}</div>
+            <button className="pointer" onClick={() => increment(item.id)}>
+              +
+            </button>
+          </div>
+          <div className="flex items-center">${item.price}</div>
         </div>
       ))}
       {cartItems.length}
